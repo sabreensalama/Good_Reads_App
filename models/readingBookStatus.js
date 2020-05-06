477bf7a78141e34a2b5c0f7d8d4762f055494ffc
MohamedAbouserei <medoaboserii@gmail.com>
2020-05-06 19:51:19 +0200

    fixed issues

--- models/readingBookStatus.js
+++ models/readingBookStatus.js
@@ -4,13 +4,13 @@ var bookStatusSchema = new mongoose.Schema({
     user:{type: Schema.Types.ObjectId, ref: 'User', required: true},
     book:{type: Schema.Types.ObjectId, ref: 'Book', required: true} ,
     status:{ type: String , enum:[ "reading" , "want to read" , "finished reading" ]}
-    // 1 read , 2 currently read , 3 want to read
-
     });
+    
     bookStatusSchema.pre('find', function (next) {
         this.populate('user')
         this.populate('book')
         next()
         })
+        bookStatusSchema.index({user: 1, book: 1}, {unique: true});
  const readingStatusModel =  mongoose.model('ReadingStatus', bookStatusSchema);
  module.exports = readingStatusModel
